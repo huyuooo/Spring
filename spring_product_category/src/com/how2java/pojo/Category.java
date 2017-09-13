4f40046499555daeafae894dc71f9e8339109c5a
HuYu <huyu1029@163.com>
2017-09-13 20:22:18 +0800

    product_category

---
+++ spring_product_category/src/com/how2java/pojo/Category.java
@@ -0,0 +1,22 @@
+package com.how2java.pojo;
+
+public class Category {
+	
+	private int id;
+	private String name;
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	public int getId(){
+		return id;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+	
+}
