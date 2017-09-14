1f4ed8024d28303c516954cdafb59589c398db76
HuYu <huyu1029@163.com>
2017-09-14 10:31:01 +0800

    AOP

---
+++ spring_aop/src/com/how2java/pojo/Category.java
@@ -0,0 +1,25 @@
+package com.how2java.pojo;
+
+import org.springframework.stereotype.Component;
+
+//@Component("c")
+public class Category {
+	
+	private int id;
+	private String name;
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	public int getId(){
+		return id;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+	
+}
---
+++ spring_aop/src/com/how2java/test/TestSpring.java
@@ -0,0 +1,41 @@
+package com.how2java.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.how2java.pojo.Category;
+import com.how2java.pojo.Product;
+import com.how2java.service.ProductService;
+
+public class TestSpring {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[] { "applicationContext.xml" });
+
+		Category c = (Category) context.getBean("c");
+
+		System.out.println(c.getId());
+		System.out.println(c.getName());
+		
+		/*
+		Category c2 = new Category();
+		c2.setId(2);
+		c2.setName("category2");
+		
+		System.out.println(c2.getId());
+		System.out.println(c2.getName());
+		*/
+		Product p = (Product) context.getBean("p");
+		
+		System.out.println(p.getId());
+		System.out.println(p.getName());
+		System.out.println(p.getPrice());
+		
+		System.out.println(p.getCategory().getId());
+		System.out.println(p.getCategory().getName());
+		
+		ProductService s = (ProductService) context.getBean("s");
+		s.doSomeService();
+	}
+
+}
